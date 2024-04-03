cc959ae13cd930afaf8f38d2eed5e8b00ecb34b3
Gryphon Miller <95644173+GaliEXE@users.noreply.github.com>
2024-04-03 16:43:41 -0500

    Added Ashen Wood Log Rotation/ Added Ashenwood planks and slabs. Ashenwood Crafting and mining currently are operational.

--- src/main/java/net/gali/havenox/block/ModBlocks.java
+++ src/main/java/net/gali/havenox/block/ModBlocks.java
@@ -1,26 +1,35 @@
 package net.gali.havenox.block;
 
+import com.mojang.serialization.MapCodec;
+import net.fabricmc.fabric.api.block.v1.FabricBlockState;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.gali.havenox.Havenox;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+
     public static final Block ASHENWOOD_LOG = registerBlock("ashenwood_log",
-            new Block(FabricBlockSettings.copyOf(Blocks.CRIMSON_HYPHAE)));
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.CRIMSON_HYPHAE)));
+    public static final Block ASHENWOOD_PLANKS = registerBlock("ashenwood_planks",
+            new Block(FabricBlockSettings.copyOf(Blocks.CRIMSON_PLANKS)));
+
+    public static final Block ASHENWOOD_SLAB = registerBlock("ashenwood_slab",
+            new SlabBlock(FabricBlockSettings.copyOf(Blocks.CRIMSON_SLAB)));
+    
 
     private static void addItemsToItemGroup(FabricItemGroupEntries entries){
         entries.add(ASHENWOOD_LOG);
+        entries.add(ASHENWOOD_PLANKS);
+        entries.add(ASHENWOOD_SLAB);
     }
 
     private static Block registerBlock(String name, Block block){
